7b6355165bac74225f92cbe61020d45fa903a66e
Tom <td00@protonmail.com>
2019-08-19 17:58:12 +0200

    Deleted

--- touch-script2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#find /home/tom/TorrentShare/RadarrDownloads/ -exec touch {} +
-find /home/tom/TorrentShare/RadarrDownloads -cmin -5 '!' -name "*.partial~" -exec touch -c {} +
-find /home/tom/TorrentShare/SonarrDownloads -cmin -5 '!' -name "*.partial~" -exec touch -c {} +
